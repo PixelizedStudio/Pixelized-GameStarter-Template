679c7f020f7810445de2a1f4dffb3f428aed0b60
Yusuf Kerem <47509437+yusufkerem@users.noreply.github.com>
2021-05-14 16:31:31 +0300

    Initial Commit

---
+++ StartingPack/Assets/GamePix/CamFollow.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+    public string TargetTag;
+
+    private Transform target;
+
+    public float smoothSpeed;
+
+    public Vector3 offset;
+
+    
+
+    void Update()
+    {
+
+        CameraFollow();
+
+    }
+    void CameraFollow()
+    {
+        target = GameObject.FindGameObjectWithTag(TargetTag).gameObject.transform;
+
+
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+
+    }
+    
+
+    
+
+    
+}
---
+++ StartingPack/Assets/GamePix/GamePix.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace GamePixLibrary
+{
+    public static class GamePix
+    {
+        public static void MoveTo(GameObject obj1, GameObject obj2,float speed)
+        {
+            //call in update
+            obj1.transform.position = Vector3.Lerp(obj1.transform.position, new Vector3(obj2.transform.position.x, obj2.transform.position.y, obj2.transform.position.z), speed);
+            obj1.transform.rotation = Quaternion.Lerp(obj1.transform.rotation, obj2.transform.rotation, speed);
+        }
+
+        public static void MoveForward(GameObject obj,float speed)
+        {
+            obj.transform.position += obj.transform.forward * Time.deltaTime * speed;
+        }
+
+        public static void MoveBackward(GameObject obj, float speed)
+        {
+            obj.transform.position -= obj.transform.forward * Time.deltaTime * speed;
+        }
+
+
+    }
+}
+

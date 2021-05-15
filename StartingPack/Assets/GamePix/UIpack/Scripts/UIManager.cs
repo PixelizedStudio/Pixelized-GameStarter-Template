363f35ac90156dea2f5d4785e382cebad0fb2760
Yusuf Kerem <47509437+yusufkerem@users.noreply.github.com>
2021-05-15 16:29:49 +0300

    Added UI Pack

---
+++ StartingPack/Assets/GamePix/UIpack/Scripts/UIManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Header("Menu Panels")]
+    public GameObject mainPanel;
+    public GameObject gamePanel;
+    public GameObject failPanel;
+    public GameObject winPanel;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RetryLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void StartGame()
+    {
+        mainPanel.SetActive(false);
+        gamePanel.SetActive(true);
+    }
+
+    public void LevelFail()
+    {
+        gamePanel.SetActive(false);
+        failPanel.SetActive(true);
+    }
+
+    public void LevelComplete()
+    {
+        gamePanel.SetActive(false);
+        winPanel.SetActive(true);
+    }
+}
